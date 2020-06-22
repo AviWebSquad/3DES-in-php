e3d8fdc49809101ca930a67874190bd4e547003f
AvinashKishan <avinas9624@gail.com>
2020-06-22 15:40:44 -0700

    done

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $des = new Crypt_TripleDES();
 //Set the Key 
 $des->setKey('abcdefghijklmnopqrstuvwx');
 //Normal Text
-echo "<h2>Triple DES algorithm example in PHP By clever geeks</h2>";
+echo "<h2>Triple DES algorithm example in PHP By clever geeks </h2>";
 $plain="Clever Geeks";
 echo "<br><b>Plain Text:- </b>".$plain."<br>";
 $enc= $des->encrypt($plain);
